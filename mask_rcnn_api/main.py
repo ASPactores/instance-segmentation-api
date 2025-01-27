7627f554956eb0b7ef473ae2d4b2af5cb80d8b14
ASPactores <anakin.pactores@gmail.com>
2025-01-27 16:31:30 +0800

    feat(mask-rcnn-api): implement initial API structure

--- mask_rcnn_api/main.py
+++ mask_rcnn_api/main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+import io
+import base64
+import cv2
+import numpy as np
 
-app = FastAPI()
+from fastapi import FastAPI, APIRouter, UploadFile, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from PIL import Image
+from ultralytics import YOLO
+
+
+app = FastAPI(
+    title="Mask R-CNN API",
+    root_path="/api/mask-rcnn-inference",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class LoculeCount(BaseModel):
+    photo: bytes
+    count: int
 
 @app.get("/")
 def read_root():
     return {"Hello": "From Mask R-CNN API!"}
+
+@app.post("/predict/", response_model=LoculeCount)
+async def count_locules(input: UploadFile) -> LoculeCount:
+    pass
